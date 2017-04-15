161bebb1f649aef2530db20bd7fffd64fbe8d5aa
August Lautt <adlautt@gmail.com>
2017-04-15 18:17:14 -0400

    Refactored code for functions and introduced bugs in those functions

--- projects/lautta/dominion/dominion.h
+++ projects/lautta/dominion/dominion.h
@@ -130,4 +130,11 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+// REFACTORED CARD EFFECT FUNCTIONS
+int playAdventurer(int drawntreasure, struct gameState *state, int currentPlayer, int cardDrawn, int temphand[], int z);
+int playCouncil_Room(int currentPlayer, struct gameState *state, int handPos);
+int playSmithy(int currentPlayer, struct gameState *state, int handPos);
+int playVillage(int currentPlayer, struct gameState *state, int handPos);
+int playGreat_Hall(int currentPlayer, struct gameState *state, int handPos);
+
 #endif
